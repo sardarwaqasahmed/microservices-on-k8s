4924c7276cb30a4689a2c38399e9e1ccb057335e
sardarwaqasahmed <architect_pakistan@hotmail.com>
2021-06-21 00:32:29 +0400

    front end service initiated

---
+++ stakater-fe/src/main/java/com/demo/stakater/StakaterApiFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.demo.stakater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StakaterApiFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StakaterApiFrontEndApplication.class, args);
+	}
+
+}
---
+++ stakater-fe/src/test/java/com/demo/stakater/StakaterApiFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.stakater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StakaterApiFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
